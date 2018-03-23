d838cf83d059ce319263bd6d5262bb5eac835762
Joe Sharp <joe.martin.sharp@gmail.com>
2018-03-23 08:13:26 +0000

    fixing the tree

--- britta/sketch.js
+++ britta/sketch.js
@@ -1,29 +1,34 @@
 const WIDTH = 500
 const HEIGHT = 500
 const BRANCH_INITIAL_LENGTH = 100
+const BRANCH_INITIAL_THICKNESS = 30
+const FRAME_RATE = 30
+const FRAME_TIME = 1 / FRAME_RATE
 
 let tree;
+let frameTime = 0
 
 function setup() {
   createCanvas(WIDTH, HEIGHT);
-  tree = new Branch(createVector(WIDTH / 2, HEIGHT), BRANCH_INITIAL_LENGTH, 0)
+  tree = branchAt(createVector(WIDTH / 2, HEIGHT))
+    .withLength(BRANCH_INITIAL_LENGTH)
+    .withDirection(0)
+    .withThickness(BRANCH_INITIAL_THICKNESS)
+    .build()
 }
 
 let printed = false
 
 function draw() {
   background('skyblue')
-  stroke('black')
+  stroke('darkgreen')
   
-  if (!printed) {
-    console.log('Branches', tree)
-  }
   tree.iterateBranches(branch => {
-    if (!printed) {
-      console.log('fuck yeah', branch)
-    }
-    line(branch.begin.x, branch.begin.y, branch.end.x, branch.end.y)
+    const jitter = 0;// map(noise(frameTime), 0, 1, -50, 50)
+    strokeWeight(branch.thickness)
+    line(branch.begin.x + jitter, branch.begin.y, branch.end.x, branch.end.y)
   })
 
+  frameTime += FRAME_TIME
   printed = true;
 }
--- britta/tree.js
+++ britta/tree.js
@@ -1,18 +1,62 @@
-class Branch {
-    constructor(begin, length, direction) {
-        this.begin = begin
-        let end = createVector(0, length)
-        end.rotate(direction)
-        this.end = p5.Vector.sub(this.begin, end)
-        this.length = length
+const LENGTH_REDUCTION = 0.67
+const THICKNESS_REDUCTION = 0.67
 
+class BranchBuilder {
+    constructor(begin) {
+        this.begin = begin;
+        this.length = 100;
+        this.direction = 0;
+        this.thickness = 20;
+    }
+
+    withLength(l) {
+        this.length = l;
+        return this;
+    }
+
+    withDirection(l) {
+        this.direction = l;
+        return this;
+    }
+
+    withThickness(l) {
+        this.thickness = l;
+        return this;
+    }
+
+    build() {
+        return new Branch(this);
+    }
+}
+
+function branchAt(begin) {
+    return new BranchBuilder(begin)
+}
+
+class Branch {
+    constructor(builder) {
+        const ANGLE_SPREAD = PI / 5
+
+        this.begin = builder.begin
+        this.length = builder.length
+        this.direction = builder.direction
+        this.thickness = builder.thickness
         this.branches = []
 
-        if (this.length > 10) {
-            let newDirection = direction - PI / 6
+        // Calculate end
+        let end = createVector(0, this.length)
+        end.rotate(this.direction)
+        this.end = p5.Vector.sub(this.begin, end)
+
+        if (this.length > 2) {
+            let newDirection = this.direction - ANGLE_SPREAD
             for (let x=0; x<2; x++) {
-                this.branches.push(new Branch(this.end.copy(), this.length * 0.67, newDirection))
-                newDirection += PI / 3
+                this.branches.push(branchAt(this.end)
+                        .withLength(this.length * LENGTH_REDUCTION)
+                        .withDirection(newDirection)
+                        .withThickness(this.thickness * THICKNESS_REDUCTION)
+                        .build())
+                newDirection += 2 * ANGLE_SPREAD
             }
         }
     }
