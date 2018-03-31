9e741af7d6e145824d90c43cc668a9f9c44ec983
Joe Sharp <joe.martin.sharp@gmail.com>
2018-03-31 15:46:21 +0100

    fully configure supershapes, maths still suspect

--- supershapes/sketch.js
+++ supershapes/sketch.js
@@ -4,74 +4,125 @@ const RESOLUTION = 100;
 let cameraX = 0;
 let cameraY = 0;
 
-let radiusSlider;
-let mSlider, n1Slider, n2Slider, n3Slider;
-let mCheckbox, n1Checkbox, n2Checkbox, n3Checkbox;
+let configCapture;
 
-const DEFAULT_M = 6;
-const DEFAULT_N1 = 60;
-const DEFAULT_N2 = 55;
-const DEFAULT_N3 = 1000;
+const DEFAULT_RADIUS = 100;
+const DEFAULT_M = 7;
+const DEFAULT_N1 = 0.2;
+const DEFAULT_N2 = 1.7;
+const DEFAULT_N3 = 1.7;
+
+class FloatCapture {
+  constructor(name, defaultValue) {
+    this.label = createSpan(name)
+    this.input = createInput(defaultValue, 'number');
+    this.value = defaultValue;
+    this.input.input(() => {
+      this.value = parseFloat(this.input.value());
+    });
+  }
+
+  getValue() {
+    return this.value;
+  }
+}
+
+class ValueCapture {
+  constructor(name, defaultValue) {
+    this.name = name;
+
+    this.value = defaultValue;
+    this.label = createP(name)
+    this.fixedCapture = new FloatCapture('Fixed', defaultValue);
+    this.minCapture = new FloatCapture('Min', defaultValue);
+    this.maxCapture = new FloatCapture('Max', defaultValue);
+
+    this.oscillateCheckbox = createCheckbox(name, false);
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  animate(time) {
+    if (this.oscillateCheckbox.checked()) {
+      this.value = map(sin(time), 0, 1, this.minCapture.getValue(), this.maxCapture.getValue());
+    } else {
+      this.value = parseFloat(this.fixedCapture.getValue());
+    }
+  }
+}
+
+class ShapeConfigCapture {
+  constructor(name) {
+    this.label = createP(name)
+    this.m = new ValueCapture('m', DEFAULT_M)
+    this.n1 = new ValueCapture('n1', DEFAULT_N1)
+    this.n2 = new ValueCapture('n2', DEFAULT_N2)
+    this.n3 = new ValueCapture('n3', DEFAULT_N3)
+  }
+
+  getConfig() {
+    return {
+      a: 1,
+      b: 1,
+      m: this.m.getValue(),
+      n1: this.n1.getValue(),
+      n2: this.n2.getValue(),
+      n3: this.n3.getValue(),
+    }
+  }
+
+  animate(time) {
+    this.m.animate(time);
+    this.n1.animate(time);
+    this.n2.animate(time);
+    this.n3.animate(time);
+  }
+}
+
+class MutliShapeConfigCapture {
+  constructor() {
+    this.radius = new ValueCapture('radius', DEFAULT_RADIUS)
+    this.r1 = new ShapeConfigCapture('Shape R1');
+    this.r2 = new ShapeConfigCapture('Shape R2');
+  }
+
+  getConfig() {
+    return {
+      r1: this.r1.getConfig(),
+      r2: this.r2.getConfig(),
+      radius: this.radius.getValue()
+    }
+  }
+
+  animate(time) {
+    this.radius.animate(time);
+    this.r1.animate(time);
+    this.r2.animate(time);
+  }
+}
 
 function setup() {
   createCanvas(500, 500, WEBGL);
   colorMode(HSB)
 
-  createP('Radius')
-  radiusSlider = createSlider(1, 200, 100, 1)
-
-  createP('Edit Values')
-  createSpan('m')
-  mSlider = createSlider(0, 7, DEFAULT_M, 0.1);
-  createSpan('n1')
-  n1Slider = createSlider(0.1, 1024, DEFAULT_N1, 0.1);
-  createSpan('n2')
-  n2Slider = createSlider(0.1, 1024, DEFAULT_N2, 0.1);
-  createSpan('n3')
-  n3Slider = createSlider(0.1, 1024, DEFAULT_N3, 0.1);
-
-  createP('Oscillate Values')
-  mCheckbox = createCheckbox('m', false);
-  n1Checkbox = createCheckbox('n1', false);
-  n2Checkbox = createCheckbox('n2', false);
-  n3Checkbox = createCheckbox('n3', false);
+  configCapture = new MutliShapeConfigCapture();
 }
 
 let cameraPos = 0;
-let m = 0;
 let animatePhase = 0;
-let a = 1;
-let b = 1.0;
 
-function supershape(theta, m, n1, n2, n3) {
-  let r = 1;
-
-  let t1 = (1 / a) * cos(m * theta / 4)
+function supershape(theta, config) {
+  let t1 = (1 / config.a) * cos(config.m * theta / 4)
   t1 = abs(t1)
-  t1 = pow(t1, n2)
+  t1 = pow(t1, config.n2)
   
-  let t2 = (1 / b) * sin(m * theta / 4)
+  let t2 = (1 / config.b) * sin(config.m * theta / 4)
   t2 = abs(t2)
-  t2 = pow(t2, n3)
+  t2 = pow(t2, config.n3)
 
-  return pow((t1 + t2), -1/n1);
-}
-
-function precisionRound(number, precision) {
-  var factor = Math.pow(10, precision);
-  return Math.round(number * factor) / factor;
-}
-
-function getSimulationValue(checkbox, slider) {
-  if (checkbox.checked()) {
-    let min = parseFloat(slider.elt.min);
-    let max = parseFloat(slider.elt.max);
-    let v = map(sin(animatePhase), 1, -1, min, max)
-    v = precisionRound(v,2);
-    return v;
-  } else {
-    return slider.value();
-  }
+  return pow((t1 + t2), -1/config.n1);
 }
 
 function draw() {
@@ -84,34 +135,28 @@ function draw() {
  
   const sphere = []
 
-  radius = radiusSlider.value()
-  m = getSimulationValue(mCheckbox, mSlider);
-  n1 = getSimulationValue(n1Checkbox, n1Slider);
-  n2 = getSimulationValue(n2Checkbox, n2Slider);
-  n3 = getSimulationValue(n3Checkbox, n3Slider);
-  //console.log(n2)
+  let config = configCapture.getConfig()
+  configCapture.animate(animatePhase);
   animatePhase += 0.05;
-
-  //console.log('m: ' + m + ', n1: ' + n1 + ', n2: ' + n2 + ', n3: ' + n3)
 
   // Calculate points
   for (let i=0; i < RESOLUTION+1; i++) {
     const latitude = map(i, 0, RESOLUTION, -HALF_PI, HALF_PI);
-    const r2 = supershape(latitude, m, n1, n2, n3)
+    const r2 = supershape(latitude, config.r2)
 
     const pointsAtLatitude = []
     for (let j=0; j < RESOLUTION+1; j++) {
       const longitude = map(j, 0, RESOLUTION, -PI, PI);
-      const r1 = supershape(longitude, m, n1, n2, n3)
+      const r1 = supershape(longitude, config.r1)
 
       // Sphere
       // const x = RADIUS * sin(latitude) * cos(longitude);
       // const y = RADIUS * sin(latitude) * sin(longitude);
       // const z = RADIUS * cos(latitude);
 
-      const x = radius * r1 * cos(longitude) * r2 * cos(latitude);
-      const y = radius * r1 * sin(longitude) * r2 * cos(latitude);
-      const z = radius * r2 * sin(latitude);
+      const x = config.radius * r1 * cos(longitude) * r2 * cos(latitude);
+      const y = config.radius * r1 * sin(longitude) * r2 * cos(latitude);
+      const z = config.radius * r2 * sin(latitude);
 
       // 'Hairy' sphere
       //const rawPoint = createVector(x, y, z);
@@ -158,7 +203,7 @@ function draw() {
 
 function mouseWheel(event) {
   //move the square according to the vertical scroll amount
-  cameraPos += event.delta;
+  //cameraPos += event.delta;
   //uncomment to block page scrolling
   //return false;
 }
